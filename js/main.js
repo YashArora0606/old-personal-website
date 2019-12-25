59dd4852e21c81420c73321b64bd5c32e50db090
Yash Arora <yash_arora6@hotmail.com>
2019-12-24 22:01:14 -0500

    MInor Customizations

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Yash Arora.", "An Engineer.", "A Developer.", "An Innovator."],
       typeSpeed: 100,
       loop: true,
     });
